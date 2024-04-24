38c468059e18255f51d9fe411db56a958ca7c479
Svpur <2049922472@qq.com>
2024-04-25 00:05:22 +0800

    active

--- models.py
+++ models.py
@@ -137,8 +137,13 @@ class Bert(nn.Module):
             loss_fct = nn.CrossEntropyLoss(ignore_index=0)  # 忽略padding的loss计算，假设padding标签为0
             # 这里需要对logits做适当的view处理以匹配tags的形状，但通常不需要view，直接传入序列即可
             active_loss = attention_mask.view(-1) == 1
-            active_logits = logits.view(-1, self.tagset_size)[active_loss]
-            active_labels = tags.view(-1)[active_loss]
+            # active_logits = logits.view(-1, self.tagset_size)[active_loss]
+            # active_labels = tags.view(-1)[active_loss]
+
+            active_loss_indices = torch.where(active_loss)[0]  # 获取active_loss中为True的索引
+            active_logits = logits.view(-1, self.tagset_size)[active_loss_indices]
+            active_labels = tags.view(-1)[active_loss_indices]
+
             loss = loss_fct(active_logits, active_labels)
             return loss
         else:
