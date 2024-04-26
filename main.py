15f282edde94e8e932f40966f3c409973cefdd47
Svpur <2049922472@qq.com>
2024-04-26 11:18:03 +0800

    up

--- main.py
+++ main.py
@@ -113,16 +113,16 @@ def test(model, iterator, device):
             x = x.to(device)
             z = z.to(device)
             y_hat = model(x, y, z, is_test=True)
-            mask = (z==1).gpu()
+            mask = (z==1).cpu()
             # Save prediction
-            y_hat_orig = torch.masked_select(y_hat, mask)
-            Y_hat.append(y_hat_orig.cpu())
+            y_hat_orig = torch.masked_select(y_hat.cpu(), mask)
+            Y_hat.append(y_hat_orig)
             # for j in y_hat:
             #   Y_hat.extend(j)
             # Save labels
             # mask = (z==1).cpu()
             y_orig = torch.masked_select(y, mask)
-            Y.append(y_orig.cpu())
+            Y.append(y_orig)
 
     Y = torch.cat(Y, dim=0).numpy()
     y_true = [idx2tag[i] for i in Y]
