7522af51ba794444f8303e02953f3234e56c5bf2
Svpur <2049922472@qq.com>
2024-04-27 09:17:38 +0800

    up

--- main0.py
+++ main0.py
@@ -79,6 +79,8 @@ def validate(e, model, iterator, device):
 
             print("logits:", logits.shape)
             print("logits[0]:", logits[0].shape)
+            print("logits[1]:", logits[1].shape)
+            print("logits[2]:", logits[2].shape)
             # 过滤掉特殊token及padding的token
             logits_clean = logits.transpose(1, 2)[y != 0]
             print("logits_clean:", logits_clean.shape)
