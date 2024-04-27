6bccb147c98346216068b4b31c4497d3d90ea97a
Svpur <2049922472@qq.com>
2024-04-27 11:55:51 +0800

    up

--- main0.py
+++ main0.py
@@ -77,16 +77,24 @@ def validate(e, model, iterator, device):
             z = z.to(device)
             loss, logits = model(x,y,z)
             print("y:", y.shape)
+            print("z:", z.shape)
             print("logits:", logits.shape)
-            print("logits[0]:", logits[0].shape)
+            # print("logits[0]:", logits[0].shape)
             # print("logits[1]:", logits[1].shape)
             # print("logits[2]:", logits[2].shape)
             # 过滤掉特殊token及padding的token
-            logits_clean = logits[0].transpose(0, 1)[y != 0]
-            print("logits_clean:", logits_clean.shape)
-            label_clean = y[y != 0]
             # 获取最大概率值
-            predictions = logits_clean.argmax(dim=0)
+            logits_max = logits.argmax(dim=2)
+            print("logits_max:", logits_max.shape)
+            logits_clean = logits_max[y != 0]
+            print("logits_clean:", logits_clean.shape)
+            M = logits_max[z]
+            print("M:", M.shape)
+
+            label_clean = y[y != 0]
+            print("label_clean:", label_clean.shape)
+    
+            predictions = logits_clean
 
             losses += loss.item()
 
